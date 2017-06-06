585bd46ea2a4c7f26dab2dad4d31752ec2e8e472
AleeWeb <aalexanderpr@gmail.com>
2017-06-06 19:05:20 -0400

    Starter function code

--- tennis.js
+++ tennis.js
@@ -3,3 +3,12 @@ var canvas = document.getElementById('tgame');
 
 canvas.width =  800;
 canvas.height = 600;
+
+function drawEverything() {
+  canvasContext.fillStyle = 'black';
+  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+  canvasContext.fillStyle = 'white';
+  canvasContext.fillRect(255,210, 200,200);
+  canvasContext.fillStyle = 'red';
+  canvasContext.fillRect(ballX, 200, 50, 25);
+}
